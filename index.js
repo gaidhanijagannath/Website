a4fae4adc15714bcb39b7c78b17b3af28285b0ae
Jagannath Gaidhani <jagannathgaidhani@gmail.com>
2018-02-13 01:24:27 +0530

    nav bar animation

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// nav bar
-$(document).ready(function(){
 
-  $(window).on('scroll',function(){
-    if($(window).scrollTop()){
-      $('.mynav').addClass('black');
-      $('.mynav').slideDown();
-    }else{
-      $('.mynav').removeClass('black');
+$(document).ready(function(e) {
+  $("nav").before($(".mynav").clone().addClass("fixed"));
+  $(window).on('scroll', function() {
 
+    if ($(window).scrollTop()>=200) {
+      $('.mynav').addClass('black slideDown');
+    } else {
+      $('.mynav').removeClass('black slideDown');
     }
   });
 });
